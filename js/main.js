61d7ca9935208b69fa285385ae94fe675f7cd68b
Ernauzer <Ernauzer177@gmail.com>
2020-09-18 00:42:13 +0300

    fst

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(".hamburger_link").click(function (e) {
+  e.preventDefault();
+  $(".nav").toggleClass("menu_active").slideToggle(500);
+  $(".hamburger_link").toggleClass("active");
+
+  $("nav").hasClass("menu_active")
+    ? $("body").attr("style", "overflow: hidden")
+    : $("body").removeAttr("style", "overflow: hidden");
+});
+
+$(".rooms_btn_filter").click((e) => {
+  e.preventDefault();
+  $(".filter_wrapper").toggleClass("filter_wrapper_active");
+
+  $(".filter_wrapper").hasClass("filter_wrapper_active")
+    ? $("body").attr("style", "overflow: hidden")
+    : $("body").removeAttr("style", "overflow: hidden");
+});
+
+$(".btn_pop-up").click((e) => {
+  e.preventDefault();
+  $(".pop-up_wrapper").addClass("pop-up_wrapper_active");
+  $(".pop-up_wrapper").hasClass("pop-up_wrapper_active")
+    ? $("body").attr("style", "overflow: hidden")
+    : $("body").removeAttr("style", "overflow: hidden");
+});
+
+function openPopUp(selectorClick, selector, newClass) {
+  $(`${selectorClick}`).click((e) => {
+    e.preventDefault();
+    $(`${selector}`).removeClass(`${newClass}`);
+    $("body").removeAttr("style", "overflow: hidden");
+  });
+}
+
+$(function () {
+  var owl = $(".rooms_slider.owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    dotClass: "dots_custom",
+    navClass: ["prev", "next"],
+    navText: " ",
+    responsive: {
+      1366: {
+        nav: true,
+        navText: [" "],
+      },
+    },
+  });
+});
+
+$(".comment_slider.owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 30,
+  dotClass: "dots_custom",
+  navClass: ["prev", "next"],
+  navText: " ",
+  nav: false,
+  responsive: {
+    768: {
+      dots: true,
+      autoWidth: true,
+      items: 2,
+    },
+    1366: {
+      autoWidth: true,
+      items: 2,
+      nav: true,
+    },
+    2020: {
+      items: 4,
+      nav: true,
+    }
+  },
+});
+
+openPopUp(".clouse_btn", ".filter_wrapper", "filter_wrapper_active");
+openPopUp(
+  ".pop-up_conteiner .clouse_btn",
+  ".pop-up_wrapper",
+  "pop-up_wrapper_active"
+);
---
+++ js/rooms.js
@@ -0,0 +1,103 @@
+﻿const roomConteiner = document.querySelector("#rooms_body_conteiner");
+const roomsFilterBtnConteiner = document.querySelector(
+  ".rooms_body_filter_btn_conteiner"
+);
+
+fetch("./json/rooms.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let roomsArray = data;
+    getInfo(roomsArray);
+  });
+
+const createRooms = (
+  price,
+  area,
+  img320,
+  img768,
+  img1366,
+  name,
+  sizes,
+  equipmentsList
+) => {
+  roomConteiner.innerHTML += ` <div class="rooms" data-price="${price}" data-area="${area}">
+           <picture>
+               <source media="(min-width: 1366px)" srcset="${img1366}" type="image/jpeg">
+               <source media="(min-width: 768px)" srcset="${img768}" type="image/jpeg">
+               <source media="(max-width: 767px)" srcset="${img320}" type="image/jpeg">
+               <img src="${img1366}" alt="cats-room" class="rooms_logo">
+          </picture>
+          <div class="rooms_conteiner">
+               <h3 class="title rooms_title room_title_js">${name}</h3>
+               <span class="rooms_size_price room_fil_js">Размеры (ШхГхВ) - ${sizes}</span>
+               <span class="rooms_size_price price_ex room_fil_js">Площадь - ${area}</span>
+              <div class="rooms_size_price room_equipment_img">
+                <span class="price_ep room_fil_js equipment_title">Оснащение номера</span>
+              </div>
+              <span class="rooms_size_price price_ep room_fil_js">Цена за сутки: ${price}₽</span>
+              <button class="default_button button_col btn_pop-up" type="button">Забронировать</button>
+           </div>
+        </div>
+      `;
+
+  const createEquipmentImg = (arg1) => {
+    const roomEquipmentImg = document.querySelectorAll(".room_equipment_img");
+    let imgCreate = document.createElement("img");
+    imgCreate.classList.add("equipment_img");
+    imgCreate.src = `${arg1}`;
+    roomEquipmentImg.forEach((room) => {
+      room.append(imgCreate);
+    });
+  };
+  if (equipmentsList) {
+    for (const elem of equipmentsList) {
+      let elemImg = Object.values(elem)[0];
+      createEquipmentImg(elemImg);
+    }
+  }
+};
+
+const getInfo = (data) => {
+  for (let room of data) {
+    let img320 = room.images[320];
+    let img768 = room.images[768];
+    let img1366 = room.images[1366];
+    const equipmentsList = room.roomEquipment;
+    createRooms(
+      room.priceDay,
+      room.area,
+      img320,
+      img768,
+      img1366,
+      room.name,
+      room.sizes,
+      equipmentsList
+    );
+  }
+};
+
+// roomsFilterBtnConteiner.addEventListener("click", (e) => {
+//   const target = e.target;
+//   const checkers = document.querySelectorAll(".filter_area_box label");
+//   const lengRooms = document.querySelectorAll(".rooms").length;
+//   let result = "";
+
+//   if (target.id === "btn_sort_push") {
+//     roomsArray.filter((room) => {
+//       for (let i = 0; i < checkers.length; i++) {
+//         if (!checkers[i].control.checked) {
+//           return room.area !== checkers[i].textContent;
+//         }
+//       }
+//     });
+//     roomConteiner.innerHTML = "";
+//     createRooms(roomsArray);
+//   }
+
+//   if (target.id === "btn_sort_cansel") {
+//     for (const item of checkers) {
+//       item.control.checked = true;
+//     }
+//     createRooms(roomsArray);
+//   }
+// });
